958353b4e4154a7d7da3361bb51ffe636e34ded0
Pamir Ghosh <pamirghosh1998@gmail.com>
2026-02-02 23:55:59 +0530

    Added index.html and render that

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
+jobs=[
+    {'id':1,
+     'title': 'Data Analyst',
+     'location':'Bangalore, India',
+     'Salary': '10,00000'
+     },
+     {'id':2,
+     'title': 'Backend Engineer',
+     'location':'Pune, India',
+     'Salary': '20,00000'
+     },
+     {'id':3,
+     'title': 'Data Engineer',
+     'location':'Bangalore, India',
+     'Salary': '12,00000'
+     }
+]
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html', jobs=jobs)
 if __name__=="__main__":
     app.run(debug=True)
