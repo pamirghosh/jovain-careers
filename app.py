7603c71215af77b9e67fe0beb59abdd2692e3651
pamirghosh <58973389+pamirghosh@users.noreply.github.com>
2026-02-06 00:01:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
-DB_SSL_CA = os.getenv("DB_SSL_CA")
+DB_SSL_CA=/etc/secrets/ca.pem
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = (
@@ -22,16 +22,17 @@ app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
         }
     }
 }
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-with app.app_context():
+@app.route("/")
+def home():
+    with app.app_context():
     result = db.session.execute(text("SELECT * FROM jobs"))
     rows = result.fetchall()
     jobs = [dict(row._mapping) for row in rows]
     print(jobs)
-@app.route("/")
-def home():
     return render_template('index.html',jobs=jobs)
 if __name__=="__main__":
     app.run(debug=True)
