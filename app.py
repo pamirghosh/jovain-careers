e96890f6f621d975e7cc307cb614415173faa79d
Pamir Ghosh <pamirghosh1998@gmail.com>
2026-02-05 23:53:30 +0530

    Deploy the project on render, connect with MySQL and use cloud database aiven

--- app.py
+++ app.py
@@ -1,25 +1,37 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+import os
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_SSL_CA = os.getenv("DB_SSL_CA")
 
 app = Flask(__name__)
-jobs=[
-    {'id':1,
-     'title': 'Data Analyst',
-     'location':'Bangalore, India',
-     'Salary': '10,00000'
-     },
-     {'id':2,
-     'title': 'Backend Engineer',
-     'location':'Pune, India',
-     'Salary': '20,00000'
-     },
-     {'id':3,
-     'title': 'Data Engineer',
-     'location':'Bangalore, India',
-     'Salary': '12,00000'
-     }
-]
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+    "connect_args": {
+        "ssl": {
+            "ca": DB_SSL_CA
+        }
+    }
+}
+
+db = SQLAlchemy(app)
+
+with app.app_context():
+    result = db.session.execute(text("SELECT * FROM jobs"))
+    rows = result.fetchall()
+    jobs = [dict(row._mapping) for row in rows]
+    print(jobs)
 @app.route("/")
 def home():
-    return render_template('index.html', jobs=jobs)
+    return render_template('index.html',jobs=jobs)
 if __name__=="__main__":
     app.run(debug=True)
